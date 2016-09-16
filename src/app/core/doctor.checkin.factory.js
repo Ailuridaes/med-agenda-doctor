af88371980607336a1d6041a61c02b96ff4cbb93
Ailuridaes <ailuridaes@gmail.com>
2016-09-16 15:28:16 -0700

    Add doctor checkin functionality

---
+++ src/app/core/doctor.checkin.factory.js
@@ -0,0 +1,75 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('doctorCheckInFactory', doctorCheckInFactory);
+
+    doctorCheckInFactory.$inject = ['$http', '$q', 'apiUrl'];
+
+    /* @ngInject */
+    function doctorCheckInFactory($http, $q, apiUrl) {
+        var service = {
+            checkIn: checkIn,
+            checkOut: checkOut,
+            getActiveCheckIns: getActiveCheckIns
+        };
+
+        var doctorCheckInUrl = apiUrl + 'DoctorCheckIns';
+
+        return service;
+
+        function checkIn(doctorId) {
+            var checkIn = { doctorId: doctorId }
+            var defer = $q.defer();
+
+            $http({
+                method: 'POST',
+                url: doctorCheckInUrl,
+                data: checkIn
+            }).then(
+                function(res) {
+                    defer.resolve(res.data);
+                }, function(res) {
+                    defer.reject(res);
+                }
+            );
+            return defer.promise;
+        }
+
+        function checkOut(checkIn) {
+            var defer = $q.defer();
+
+            $http({
+                method: 'POST',
+                url: doctorCheckInUrl + '/Checkout/' + checkIn.doctorCheckInId,
+                data: checkIn
+            }).then(
+                function(res) {
+                    defer.resolve(res.data);
+                }, function(res) {
+                    defer.reject(res);
+                }
+            );
+            return defer.promise;
+        }
+
+        function getActiveCheckIns() {
+            var defer = $q.defer();
+
+            $http({
+                method: 'GET',
+                url: doctorCheckInUrl + '/Active'
+            }).then(
+                function(res) {
+                    defer.resolve(res.data);
+                }, function(res) {
+                    defer.reject(res);
+                }
+            );
+
+            return defer.promise;
+        }
+
+    }
+})();
